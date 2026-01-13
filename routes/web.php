8156fec59b385297434a6441f65c4e92d293d447
Wildan Devanata <111202214593@mhs.dinus.ac.id>
2026-01-13 21:11:42 +0700

    modul 9 done

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController;
-
+use App\Http\Controllers\Admin\EventController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,6 +24,8 @@ Route::middleware('auth')->group(function () {
 
         // Category Management
         Route::resource('categories', CategoryController::class);
+        // Event Management
+        Route::resource('events', EventController::class);
     });
         });
 require __DIR__.'/auth.php';
